a67a168889498902cdc2b49bc5ec4683dd9f200e
Valeria <valerydelgad@gmail.com>
2025-03-25 13:56:22 -0600

    Main

---
+++ Arreglos.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Arreglos{
+
+    public static void main(String[] args) {
+        double[] calificaciones = new double[5];
+        Scanner sc = new Scanner(System.in);
+        for (int i=0; i<calificaciones.length; i++){
+            System.out.println("Escribe calificacion  [" + i + "]: ");
+            calificaciones[i] = sc.nextDouble();
+        }
+        //No se imprime el contenido de todas las casillas del arreglo
+        //solo se imprimen la direccion de memoria en donde empieza el arreglo
+        //System.out.println("calificaciones: " + calificaciones);
+
+        //impresion usando el for extenido
+        System.out.println("Contenido del arreglo calificaciones: ");
+        for(double calificacion : calificaciones)
+        System.out.println(calificacion);
+
+        System.out.println("contenido de calificaiones: ");
+        //impresion con el for tradidiconal
+        for ( int i=0; i<calificaciones.length; i++)
+        System.out.println(calificaciones[i]);
+        sc.close();
+    }
+}
