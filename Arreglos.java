26116863ba703ca2e94c616366ba648690913fea
Valeria <valerydelgad@gmail.com>
2025-03-26 09:28:34 -0600

    Adding arrays vars

--- Arreglos.java
+++ Arreglos.java
@@ -2,7 +2,22 @@ import java.util.Scanner;
 
 public class Arreglos{
 
+    public static int T = 25;
+
+    public static void leerCalificaciones(){
+
+    }
+
+    public static void evaluarCalificaciones(){
+        
+    }
+
     public static void main(String[] args) {
+        double[] parcial1, parcial2, parcial3, parcial4; //solo declarando arreglos
+        double[] promInd;
+        boolean[] aprobados;
+
+
         double[] calificaciones = new double[5];
         Scanner sc = new Scanner(System.in);
         for (int i=0; i<calificaciones.length; i++){
